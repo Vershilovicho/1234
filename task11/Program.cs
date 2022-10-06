f3774e87a34b5849a15afc969166edc327935f94
Vershilovicho <vernik281186@gmail.com>
2022-10-06 17:21:38 +0300

    Добавили task11

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int twoDigit = number / 10;
+int secontDigit = twoDigit % 10;
+
+Console.WriteLine($"Вторая цифра числа: {secontDigit}");
